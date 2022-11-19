f6a0283046adb6b7ecc4b920392f25a4fa033680
Ben Merryman <ben.merryman@gmail.com>
2022-11-19 12:27:25 -0500

    testing added

--- test.js
+++ test.js
@@ -1,28 +1,48 @@
-const testTable = document.querySelectorAll("table")
-const testRows = document.querySelectorAll("tr")
+// 🌐 Global element references for passing tests
+const testTable = document.querySelectorAll("table");
+const testRows  = document.querySelectorAll("tr");
+
+// 🌐 Global variables for passing tests
+let tableCheckPass  = false;
+let rowCheckPass    = false;
+let columnCheckPass = false;
 
 function checkTable() {
-    if (testTable.length !== 1) {
-        console.log("🚫 Test failed: 1 Table element needed")
-    } else {
-        console.log("✅ Test passed! 1 Table element found")
-    }
+  if (testTable.length !== 1) {
+    console.log("🚫 Table test failed: 1 Table element needed");
+  } else {
+    console.log("✅ Table test passed! 1 Table element found");
+    tableCheckPass = true;
+  }
 }
 function checkRows() {
-    if (testTable[0].rows.length !== 3) {
-        console.log("🚫 Test failed: 3 tr elements needed")
-    } else {
-        console.log("✅ Test passed! 3 tr elements found")
-
-    }
+  let testPassed = false;
+  try{testPassed = (testTable[0].rows.length === 3)
+	} catch(e){testPassed = false
+	} finally {
+		if (testPassed) {
+			console.log("✅ Rows test passed! 3 tr (rows) elements found");
+			rowCheckPass = true;
+		} else {
+			console.log("🚫 Rows test failed: 3 tr elements needed");
+			rowCheckPass = false;
+		}
+  }
 }
 function checkColumns() {
-    let columnCheckPass = false
-    for (let i = 0; i < testTable[0].rows.length; i++) {
-        if (testTable[0].rows[0].cells.length !== 3) {
-            console.log("🚫 Test failed: 3 td elements needed")
-        } else {
-            console.log("✅ Test passed! 3 td elements found")
-        }
+  for (let i = 0; i < 3; i++) {
+    let testPassed = false;
+    try {testPassed = testTable[0].rows[i].cells.length === 3
+		} catch (e) {
+      testPassed = false;
+    } finally {
+      if (testPassed) {
+				console.log(`✅ Row ${i + 1} check passed! 3 td (columns) in each row found`);
+        columnCheckPass = true;
+      } else {
+				console.log(`🚫 Row ${i + 1} check failed: 3 td (columns) elements needed`);
+        columnCheckPass = false;
+      }
     }
+  }
 }
