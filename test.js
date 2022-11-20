07164c4ba4d08364748817c075a6a08dcd709ec6
Ben Merryman <ben.merryman@gmail.com>
2022-11-20 00:38:18 -0500

    refactor for gridTests()

--- test.js
+++ test.js
@@ -5,44 +5,54 @@ let testTable = document.querySelectorAll("table");
 let tableCheckPass  = false;
 let rowCheckPass    = false;
 let columnCheckPass = false;
-
-function checkTable() {
-  testTable = document.querySelectorAll("table")
-  if (testTable.length !== 1) {
-    console.log("🚫 Table test failed: 1 Table element needed");
-  } else {
-    console.log("✅ Table test passed! 1 Table element found");
-    tableCheckPass = true;
+// 3x3 grid tests
+function gridTests() {
+  checkTable()
+  checkRows()
+  checkColumns()
+  function checkTable() {
+    testTable = document.querySelectorAll("table")
+    if (testTable.length !== 1) {
+      console.log("🚫 Table test failed: EXACTLY 1 Table element needed");
+    } else {
+      console.log("✅ Table test passed! EXACTLY 1 Table element found");
+      tableCheckPass = true;
+    }
   }
-}
-function checkRows() {
-  let testPassed = false;
-  try{testPassed = (testTable[0].rows.length === 3)
-	} catch(e){testPassed = false
-	} finally {
-		if (testPassed) {
-			console.log("✅ Rows test passed! 3 tr (rows) elements found");
-			rowCheckPass = true;
-		} else {
-			console.log("🚫 Rows test failed: 3 tr elements needed");
-			rowCheckPass = false;
-		}
-  }
-}
-function checkColumns() {
-  for (let i = 0; i < 3; i++) {
+  function checkRows() {
     let testPassed = false;
-    try {testPassed = testTable[0].rows[i].cells.length === 3
-		} catch (e) {
-      testPassed = false;
+    try{testPassed = (testTable[0].rows.length === 3)
+    } catch(e){testPassed = false
     } finally {
       if (testPassed) {
-				console.log(`✅ Row ${i + 1} check passed! 3 td (columns) in each row found`);
-        columnCheckPass = true;
+        console.log("✅ Rows test passed! EXACTLY 3 tr (rows) elements found");
+        rowCheckPass = true;
       } else {
-				console.log(`🚫 Row ${i + 1} check failed: 3 td (columns) elements needed`);
-        columnCheckPass = false;
+        console.log("🚫 Rows test failed: EXACTLY 3 tr (rows) elements needed");
+        rowCheckPass = false;
       }
     }
   }
-}
+  function checkColumns() {
+    for (let i = 0; i < 3; i++) {
+      let testPassed = false;
+      try {testPassed = testTable[0].rows[i].cells.length === 3
+      } catch (e) {
+        testPassed = false;
+      } finally {
+        if (testPassed) {
+          console.log(`✅ Row ${i + 1} check passed! 3 td (cells or columns or data) in each row found`);
+          columnCheckPass = true;
+        } else {
+          console.log(`🚫 Row ${i + 1} check failed: 3 td (cells or columns or data) elements needed`);
+          columnCheckPass = false;
+        }
+      }
+    }
+  }
+}  
+
+
+
+
+
